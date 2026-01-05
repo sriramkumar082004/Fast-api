968157ef52490637b1488cb0fb90dceccfe30bff
Sriram Kumar <sriramkumar082004@gmail.com>
2026-01-05 19:05:58 +0530

    Add API URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*"
+        "https://react-vite-deploy-umber-psi.vercel.app/"
     ],  # Allow all origins for deployment (restrict in production if needed)
     allow_methods=["*"],
     allow_headers=["*"],
