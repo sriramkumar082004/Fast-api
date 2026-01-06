0e69143a8648d1bdee22f539fc463da875ecc7ff
Sriram Kumar <sriramkumar082004@gmail.com>
2026-01-06 09:22:56 +0530

    Allow origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,18 +6,21 @@ from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
- origins=[
-        "https://localhost:5173",
-        
-    ]
+origins = [
+    "http://localhost:5173",
+]
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
